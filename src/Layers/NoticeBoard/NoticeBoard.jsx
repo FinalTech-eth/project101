0628c982a51fea2da98322cbb03780726d85ba9e
unknown <segniworku1992@gmail.com>
2023-06-09 21:14:43 +0300

    initial commit

---
+++ src/Layers/NoticeBoard/NoticeBoard.jsx
@@ -0,0 +1,10 @@
+const NoticeBoard = () => {
+  return (
+    <div className="notice-board-container">
+      <div className="img-container"></div>
+      <div className="notice-message-container"></div>
+    </div>
+  );
+};
+
+export default NoticeBoard;

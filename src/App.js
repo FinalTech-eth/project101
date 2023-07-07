2899464b47c7c82fc94090ac0004c74db6a1ae21
Engidawork <65005850+Engida2312@users.noreply.github.com>
2023-07-07 14:45:07 +0530

    auth gard put

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Index';
-import About from './Pages/About';
-import Gallery from './Pages/Gallery';
-import Error from './Pages/Error';
-import SingleEventPage from './Pages/Event/SingleEvent';
-import ClientSharedLayout from './Layers/SharedLayouts/ClientSharedLayout';
-import AdminSharedLayout from './Layers/SharedLayouts/AdminSharedLayout';
-import Dashboard from './Pages/Admin/Dashboard';
-import AddEvent from './Pages/Admin/AddEvent';
-import AdminSideBar from './Components/Admin/SideBar';
-import AllEvents from './Pages/Admin/AllEvents';
-import GalleryImages from './Pages/Admin/Gallery';
-import SignIn from './Pages/Admin/Auth/SignIn';
-import RequiredAuth from './Components/RequiredAuth';
-import AllImages from './Pages/Admin/Gallery/AllImages';
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Index";
+import About from "./Pages/About";
+import Gallery from "./Pages/Gallery";
+import Error from "./Pages/Error";
+import SingleEventPage from "./Pages/Event/SingleEvent";
+import ClientSharedLayout from "./Layers/SharedLayouts/ClientSharedLayout";
+import AdminSharedLayout from "./Layers/SharedLayouts/AdminSharedLayout";
+import Dashboard from "./Pages/Admin/Dashboard";
+import AddEvent from "./Pages/Admin/AddEvent";
+import AdminSideBar from "./Components/Admin/SideBar";
+import AllEvents from "./Pages/Admin/AllEvents";
+import GalleryImages from "./Pages/Admin/Gallery";
+import SignIn from "./Pages/Admin/Auth/SignIn";
+import RequiredAuth from "./Components/RequiredAuth";
+import AllImages from "./Pages/Admin/Gallery/AllImages";
+import "./App.css";
 
 function App() {
   return (
@@ -27,13 +27,14 @@ function App() {
             <Route path="gallery" element={<Gallery />} />
             <Route path="event/:id" element={<SingleEventPage />} />
           </Route>
-          <Route path='admin' element={<SignIn />} />
-          <Route element={<RequiredAuth />}></Route>
-          <Route path="/dashboard" element={<AdminSideBar />}>
-            <Route index element={<Dashboard />} />
-            <Route path="add-event" element={<AddEvent />} />
-            <Route path="events" element={<AllEvents />} />
-            <Route path="gallery" element={<AllImages />} />
+          <Route path="admin" element={<SignIn />} />
+          <Route element={<RequiredAuth />}>
+            <Route path="/dashboard" element={<AdminSideBar />}>
+              <Route index element={<Dashboard />} />
+              <Route path="add-event" element={<AddEvent />} />
+              <Route path="events" element={<AllEvents />} />
+              <Route path="gallery" element={<AllImages />} />
+            </Route>
           </Route>
 
           <Route path="*" element={<Error />} />

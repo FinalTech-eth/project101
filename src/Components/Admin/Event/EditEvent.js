c50a4193449102580676932160d0e4e7d70bc6cb
Engidawork <65005850+Engida2312@users.noreply.github.com>
2023-08-01 19:46:20 +0530

    update

--- src/Components/Admin/Event/EditEvent.js
+++ src/Components/Admin/Event/EditEvent.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { TextField, Button, Box, Typography } from "@mui/material";
+import { TextField, Button, Box, Typography, MenuItem } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import axios from "../../../API/axios";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SaveIcon from "@mui/icons-material/Save";
+import EventCategories from "../../../Enums/EventCategory";
+
+const uploadImage = async (image) => {
+  const formData = new FormData();
+  formData.append("image", image);
+  const response = await axios.post("/image-upload", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
 
 const EditEventForm = ({ event, onCloseModal, onFetchEvents }) => {
   const admin = JSON.parse(localStorage.getItem("admin"));
@@ -18,7 +30,7 @@ const EditEventForm = ({ event, onCloseModal, onFetchEvents }) => {
     reset,
     setValue,
   } = useForm();
-  const [selectedImage, setSelectedImage] = useState();
+  const [selectedImage, setSelectedImage] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
@@ -27,25 +39,34 @@ const EditEventForm = ({ event, onCloseModal, onFetchEvents }) => {
     setValue("description", event.description);
     setValue("location", event.location);
     setValue("date", event.date);
+    setValue("category", event.category); // Set the "category" value
   }, [event, setValue]);
 
   const handleFormSubmit = async (data) => {
     try {
       setIsSubmitting(true);
 
-      // Create a FormData object to send the image as a multipart form
+      // Upload image using the new utility function
+      let imageUrl = null;
+      if (selectedImage) {
+        imageUrl = await uploadImage(selectedImage);
+      }
+
+      // Create a FormData object to send the data as a multipart form
       const formData = new FormData();
       formData.append("title", data.title);
       formData.append("description", data.description);
       formData.append("location", data.location);
       formData.append("date", data.date);
-      formData.append("image", selectedImage);
+      formData.append("image", imageUrl || event.image); // Use previous image URL if no new image is selected
+      formData.append("category", data.category); // Add the "category" field to the FormData
+      formData.append("video-link", data["video-link"]);
 
       // Make a PUT request using Axios and the FormData
-      await axios.put(`/api/event/update/${event._id}`, formData, {
+      await axios.put(`/event/update/${event._id}`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
-          'Authorization': `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
       });
 
@@ -109,16 +130,37 @@ const EditEventForm = ({ event, onCloseModal, onFetchEvents }) => {
         helperText={errors.location && "Location is required"}
       />
       <TextField
-        label="Date"
-        type="date"
+        label="Date and time" // Update the label to "Datetime"
+        type="datetime-local" // Use "datetime-local" input type for datetime
         variant="outlined"
         {...register("date", { required: true })}
         error={!!errors.date}
-        helperText={errors.date && "Date is required"}
+        helperText={errors.date && "Date and time is required"}
         InputLabelProps={{
           shrink: true,
         }}
       />
+      <TextField
+        label="Category"
+        select
+        variant="outlined"
+        {...register("category", { required: true })}
+        error={!!errors.category}
+        helperText={errors.category && "Category is required"}
+      >
+        {Object.values(EventCategories).map((category) => (
+          <MenuItem key={category} value={category}>
+            {category}
+          </MenuItem>
+        ))}
+        <MenuItem value="other">Other</MenuItem>
+      </TextField>
+      <TextField
+        label="Video Link" // Add the "video-link" field
+        variant="outlined"
+        {...register("video-link")}
+      />
+
       <Box sx={{ mt: 2 }}>
         <label htmlFor="image-upload">
           <input
@@ -140,6 +182,12 @@ const EditEventForm = ({ event, onCloseModal, onFetchEvents }) => {
               alt="Selected"
               style={{ height: "200px" }}
             />
+          </Box>
+        )}
+        {!selectedImage && event.image && (
+          <Box mt={2}>
+            <Typography variant="subtitle1">Previous Image:</Typography>
+            <img src={event.image} alt="Previous" style={{ height: "200px" }} />
           </Box>
         )}
       </Box>
--- src/Components/Gallery/AllImagesGallery.js
+++ src/Components/Gallery/AllImagesGallery.js
@@ -1,33 +1,44 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../API/axios";
 import ModalImage from "react-modal-image";
-
 import lightTexture from "../../Assets/Images/light-texture.jpg";
 import "./gallery.css";
 import Loading from "../Loading";
+import { Pagination } from "@mui/material";
+
+const IMAGES_PER_PAGE = 10; // Number of images per page
 
 export default function AllImagesGallery() {
   const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetchImages();
-  }, []);
+    fetchImages(currentPage);
+  }, [currentPage]);
 
-  const fetchImages = async () => {
+  const fetchImages = async (page) => {
     try {
-      const response = await axios.get("/gallery");
+      const response = await axios.get(
+        `/gallery?page=${page}&limit=${IMAGES_PER_PAGE}`
+      );
       const transformedImages = response.data.items.map((item) => ({
         original: item.image,
         thumbnail: item.image,
         caption: item.caption,
       }));
       setImages(transformedImages);
+      setTotalPages(Math.ceil(response.data.totalItems / IMAGES_PER_PAGE));
       setIsLoading(false);
     } catch (error) {
       console.error(error);
       setIsLoading(false);
     }
+  };
+
+  const handlePaginationChange = (event, value) => {
+    setCurrentPage(value);
   };
 
   const containerStyle = {
@@ -56,6 +67,15 @@ export default function AllImagesGallery() {
           />
         ))}
       </div>
+      {totalPages > 1 && (
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePaginationChange}
+          color="primary"
+          className="pagination"
+        />
+      )}
     </div>
   );
 }

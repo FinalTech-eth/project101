8665d699b3b03ab740e776ba45e2cac5b8221ff9
unknown <segniworku1992@gmail.com>
2023-08-06 19:13:48 +0300

    remove ck editor from about us page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,29 +8,9 @@ const About = () => {
     document.getElementById("about-top").scrollIntoView({ behavior: "smooth" });
   }, []);
 
-  const [description, setDescription] = useState("");
   return (
     <div id="about-top">
       <AboutSection />
-      <CKEditor
-        editor={ClassicEditor}
-        data={description}
-        onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          console.log("Editor is ready to use!", editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          setDescription(data);
-          console.log("The data : ", { event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log("Blur.", editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log("Focus.", editor);
-        }}
-      />
     </div>
   );
 };
